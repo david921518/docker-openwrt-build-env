6ccf1a4682e4a1c82953016f8acab24f8f49eb78
david921518 <david921518@qq.com>
2026-05-25 04:14:27 +0800

    fix: add release/ directory
    Signed-off-by: david921518 <david921518@qq.com>

---
+++ release/v23.05.6/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone -b v23.05.6 https://git.openwrt.org/openwrt/openwrt.git openwrt-v23.05.5
+cd openwrt-v23.05.6
+
+wget https://downloads.openwrt.org/releases/23.05.6/targets/x86/64/config.buildinfo
+cp -f config.buildinfo .config
+
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+patch -p1 < ../0001_enable-ACL-and-Security-labels-by-default.patch
+
+make oldconfig
+
+make -j4 2>&1 | tee b.log
